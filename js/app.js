2ef8de3f70ca76c78d7439c9c89f78055f17a290
Cole Mecum <mecum.cole@gmail.com>
2015-08-29 11:46:48 -0500

    fixed bug on todolist that wouldn't clear completed when button pressed

--- js/app.js
+++ js/app.js
@@ -27,28 +27,31 @@ myApp.config(['$routeProvider', function($routeProvider){
 
 
 //controller for the ANGULARJS view
-myApp.controller('todoCtrl', ['$scope', function($scope){
-    $scope.getTotalTodos = function(){
+myApp.controller('todoCtrl', ['$scope', function($scope) {
+    $scope.getTotalTodos = function () {
         return $scope.todos.length
     }
 
     $scope.todos = [
-        {text:"make a website", done:true},
-        {text:'Learn AngularJS', done:true},
-        {text:'build an app', done:true},
-        {text:"make money off said app", done:false}]
+        {text: "make a website", done: true},
+        {text: 'Learn AngularJS', done: true},
+        {text: 'build an app', done: true},
+        {text: "make money off said app", done: false}]
 
-    $scope.addTodo = function(){
-        $scope.todos.push({text:$scope.formTodoText, done:false});
-        $scope.formTodoText ='';
+    $scope.addTodo = function () {
+        $scope.todos.push({text: $scope.formTodoText, done: false});
+        $scope.formTodoText = '';
     }
 
-    $scope.clearCompleted = function(){
+    $scope.clearCompleted = function () {
         //todo either implement underscore or make a custom filter function
-        $scope.todos = _.filter($scope.todos, function(todo){
-            return !todo.done;
-        })
-    }
+        var newTodo = [];
+        for (var i = 0; i < $scope.todos.length; i++)
+            {if (!$scope.todos[i].done)
+                newTodo.push($scope.todos[i]);
+            }
+        }
+    }]);
 
 
 
